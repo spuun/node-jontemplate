6090f60ec840d4b297b880b4c7741882cbfefbc9
Jon <jon@spuun.se>
2015-12-23 08:34:49 +0100

    Updated tests. Made data parameter optional.

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,38 @@
 var assert = require('assert');
+var path = require('path');
 var jontemplate = require('../lib/template.js');
 
+var p = (f) => {
+	return path.join(__dirname, 'templates', f);
+};
+
 describe('jontemplate', () => {
-  var render = jontemplate({
-    path: __dirname + '/templates/'
-  });
+  var render = jontemplate();
   it('should render var1', () => {
-    assert.equal(
-      render('render_var1', {var1: 'ok'}), 
-      'ok');
-  });
+ 		assert.equal(
+			render(p('render_var1.html'), {var1: 'ok'})
+			,'ok');
+	});
   it('should render list1', () => {
     assert.equal(
-      render('render_list1', {list1:['a', 'b', 'c']}),
+      render(p('render_list1.html'), {list1:['a', 'b', 'c']}),
      'abc'); 
   });
+	it('should render plain', () => {
+		assert.equal(
+			render(p('render_plain.html')),
+			'ok');
+	});
+	it('should render var1 express style', done => {
+		render(p('render_var1.html'), {var1: 'express'}, (err, data) => {
+			assert.equal(data, 'express');
+			done();
+		});
+	});
+	it('should render plain express style', done => {
+		render(p('render_plain.html'), (err, data) => {
+			assert.equal(data, 'ok');
+			done();
+		});
+	});
 });
